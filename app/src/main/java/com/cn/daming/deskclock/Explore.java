0f37d972442b645b593c1a57473d989184f66d55
Chael <12301145@bjtu.edu.cn>
2015-03-30 18:27:02 +0800

    version 1.6

---
+++ app/src/main/java/com/cn/daming/deskclock/Explore.java
@@ -0,0 +1,29 @@
+package com.cn.daming.deskclock;
+
+import android.app.Activity;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+
+public class Explore extends Activity implements ExploreItem.OnFragmentInteractionListener {
+    @Override
+
+    public void onFragmentInteraction(String selectText) {
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        ExploreItemDetails fg=ExploreItemDetails.newInstance(selectText);
+        ft.replace(R.id.fragment_container, fg);
+        ft.addToBackStack(null);
+        ft.commit();
+    }
+
+    @Override
+	public void onCreate(Bundle savedInstanceState) {
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        ExploreItem fg=ExploreItem.newInstance("Food","http://www.picture.com");//title and  path just for test
+        ft.add(R.id.fragment_container, fg);
+        ft.addToBackStack(null);
+        ft.commit();
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.explore);
+
+	}
+}
