7dbb0dbfe09062c88d3465450ad0bc28e3fdef30
Chael <12301145@bjtu.edu.cn>
2015-03-30 19:30:14 +0800

    version 1.16

---
+++ app/src/main/java/com/cn/daming/deskclock/ExploreItemDetails.java
@@ -0,0 +1,36 @@
+package com.cn.daming.deskclock;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by Z on 2015/3/30.
+ */
+public class ExploreItemDetails extends Fragment {
+    String title;
+    private static final String ARG_PARAM1 = "param1";
+    public static ExploreItemDetails newInstance(String title) {
+        ExploreItemDetails fragment = new ExploreItemDetails();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, title);
+        fragment.setArguments(args);
+        return fragment;
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        this.title=getArguments().getString(ARG_PARAM1);
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.explore_item_details, container, false);
+        final TextView textView=(TextView)view.findViewById(R.id.explore_item_detail_text);
+        textView.setText(this.title);
+        return view;
+    }
+}
